5a2fbb6187a52a996be44b456942543ccf8f069e
emericsoquet <soquet.emeric@gmail.com>
2024-10-31 01:10:32 +0100

    style(hero): Limitate hook's width

--- components/Hero/HeroIntro.vue
+++ components/Hero/HeroIntro.vue
@@ -1,22 +1,22 @@
 <template>
     <section class="hero">
 
-        <div class="container grid auto-rows-max content-center my-20 md:my-40">
+        <div class="container grid auto-rows-max content-center my-20 md:my-48">
             <h1>
                 <span class="hero__suptitle uppercase font-sub block">Emeric Soquet</span>
                 <span class="hero__title font-heading font-bold block">Frontend developer</span>
             </h1>
     
-            <p class="text-2xl md:text-3xl font-sub my-8 md:my-10 md:mt-16 font-light">With a taste for design and interaction. It’s crystal clear, my pursuit is to make your websites as polished as a diamond.</p>
+            <p class="text-2xl xs:text-3xl md:text-4xl font-sub my-8 md:my-10 md:mt-16 font-light lg:max-w-4xl">With a taste for design and interaction. It’s crystal clear, my pursuit is to make your websites as polished as a diamond.</p>
     
-            <div class="hero__infos grid gap-6 sm:grid-flow-col sm:gap-8">
-                <p class="hero__status uppercase md:text-lg font-heading leading-5 pl-10">
+            <div class="hero__infos grid gap-6 sm:grid-flow-col sm:gap-10 sm:justify-start md:gap-20">
+                <p class="hero__status uppercase leading-5 md:leading-5 md:text-lg font-heading pl-14">
                     Based in Paris<br />
                     Open for new opportunities
                 </p>
     
                 <div class="hero__networks pl-5 pr-4 py-3 rounded-md w-max flex items-center">
-                    <p class="font-heading font-semibold tracking-wider mr-10">Follow me</p>
+                    <p class="font-heading font-semibold tracking-wider mr-10 md:text-lg">Follow me</p>
                     <div class="hero__networks-list flex gap-2">
                         <a  v-for="(network, i) in networks"
                             :key="i"
