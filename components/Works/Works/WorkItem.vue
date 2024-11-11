6d47fc0de9132a5a13908ee799f04b55cbf0bb11
emericsoquet <soquet.emeric@gmail.com>
2024-11-11 17:21:42 +0100

    fix(palette): Add is-active class on active color

--- components/Works/Works/WorkItem.vue
+++ components/Works/Works/WorkItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <a href="#" class="work">
+    <a href="#" class="work px-3">
         <article class="flex items-center justify-center aspect-video rounded-lg">
             <h3 class="text-3xl font-bold uppercase font-body opacity-0">Eduka</h3>
             <img src="https://i.imgur.com/sWc2zeZ.png" alt="">
