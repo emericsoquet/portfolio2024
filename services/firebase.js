d27b709ddf8595216166f05a47aa09d4a9192531
emericsoquet <soquet.emeric@gmail.com>
2025-02-16 16:44:53 +0100

    Organize fetchCollection file

--- services/firebase.js
+++ services/firebase.js
@@ -1,19 +1,21 @@
+import { collection, getDocs } from "firebase/firestore";
+
 export const fetchCollection = async (collectionName) => {
+    const firestore = useNuxtApp().$firestore;
 
-    const { $firestore } = useNuxtApp();
-
-    const { collection, getDocs } = await import('firebase/firestore');
-    const data = [];
-
-    try {
-        const querySnapshot = await getDocs(collection($firestore, collectionName));
-        querySnapshot.forEach((doc) => {
-            data.push({ id: doc.id, ...doc.data() });
-        });
-        return data;
-    } catch (error) {
-        console.error(`Impossible de récupérer la collection ${collectionName} :`, error);
-        throw error;
+    if (!firestore) {
+        console.error("Firestore n'est pas initialisé !");
+        return [];
     }
 
-}
+    try {
+        const querySnapshot = await getDocs(collection(firestore, collectionName));
+        return querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data()
+        }));
+    } catch (error) {
+        console.error(`Erreur lors de la récupération de la collection ${collectionName} :`, error);
+        throw error;
+    }
+};
